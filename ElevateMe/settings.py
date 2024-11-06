bea821fc45eca125f090b25ce894d01681ba276d
ErwinLambujon <erwinlambujon1@gmail.com>
2024-11-07 01:25:09 +0800

    Update

--- ElevateMe/settings.py
+++ ElevateMe/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
